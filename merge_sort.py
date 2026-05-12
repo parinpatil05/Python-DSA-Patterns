6b5b1c0df6fcbf71e5d9d7ab44a49fe0df892d09
Parin Patil <parinvpatil@gmail.com>
2026-05-12 22:30:50 +0530

    Implement merge sort algorithm in merge_sort.py

---
+++ merge_sort.py
@@ -0,0 +1,36 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+
+    left_half = merge_sort(arr[:mid])
+    right_half = merge_sort(arr[mid:])
+
+    return merge(left_half, right_half)
+
+
+def merge(left, right):
+    sorted_array = []
+
+    i = 0
+    j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            sorted_array.append(left[i])
+            i += 1
+        else:
+            sorted_array.append(right[j])
+            j += 1
+
+    sorted_array.extend(left[i:])
+    sorted_array.extend(right[j:])
+
+    return sorted_array
+
+
+numbers = [38, 27, 43, 3, 9, 82, 10]
+
+print("Original Array:", numbers)
+print("Sorted Array:", merge_sort(numbers))
