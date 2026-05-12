096260fdd093ac0653f55009b3d0edf0d9b4ac84
Parin Patil <parinvpatil@gmail.com>
2026-05-12 22:32:25 +0530

    Add cycle detection for linked list
    Implement cycle detection in a linked list using Floyd's Tortoise and Hare algorithm.

---
+++ detect_cycle.py
@@ -0,0 +1,32 @@
+class ListNode:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+def has_cycle(head):
+    slow = head
+    fast = head
+
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+        if slow == fast:
+            return True
+
+    return False
+
+
+# Creating linked list with cycle
+head = ListNode(1)
+second = ListNode(2)
+third = ListNode(3)
+fourth = ListNode(4)
+
+head.next = second
+second.next = third
+third.next = fourth
+fourth.next = second
+
+print("Cycle Detected:", has_cycle(head))
